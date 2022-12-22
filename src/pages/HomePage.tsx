51036126c52bd7b82359ebff1259183362a4e260
krystofhorak <krystof.horak@proton.me>
2022-12-22 20:33:41 +0100

    Creating the Default layout

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,10 @@
+import DefaultLayout from '../layouts/DefaultLayout';
+
 const HomePage = (): JSX.Element =>
   (
-    <>
+    <DefaultLayout>
       <h1>Linux Ricing Generator</h1>
-    </>
+    </DefaultLayout>
   );
 
 export default HomePage;
