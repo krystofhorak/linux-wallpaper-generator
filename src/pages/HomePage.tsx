f10f5efa71d95f76b7eb8151efcec8f1ab8473f4
krystofhorak <krystof.horak@proton.me>
2022-12-22 21:33:58 +0100

    Modifying the distribution icon size

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,19 +8,30 @@ import type { Distribution } from '../typings/distroTypes';
 
 const HomePage = (): JSX.Element => {
   const [currentDistro, setCurrentDistro] = useState<Distribution>('arch');
+  const [iconSize, setIconSize] = useState<number>(1);
+
   const distroSelectRef = useRef<HTMLSelectElement | null>(null);
+  const sizeSelectRef = useRef<HTMLSelectElement | null>(null);
 
   const handleGenerate = (e: FormEvent) => {
     e.preventDefault();
-    if (!distroSelectRef.current) return;
+    if (!distroSelectRef.current || !sizeSelectRef.current) return;
     setCurrentDistro(distroSelectRef.current.value as Distribution);
+    setIconSize(Number(sizeSelectRef.current.value));
   };
 
   return (
     <DefaultLayout>
       <h1>Linux Ricing Generator</h1>
       <div className="canvas">
-        <img className="distribution-logo" src={`/distributions/${currentDistro}.svg`} alt="Arch" />
+        <img
+          className="distribution-logo"
+          src={`/distributions/${currentDistro}.svg`}
+          alt="Arch"
+          style={{
+            height: `${iconSize * 32}px`,
+          }}  
+        />
       </div>
       <h2>Generation</h2>
       <form onSubmit={handleGenerate}>
@@ -33,6 +44,12 @@ const HomePage = (): JSX.Element => {
           <option value="manjaro">Manjaro</option>
           <option value="opensuse">openSUSE</option>
           <option value="ubuntu">Ubuntu</option>
+        </select><br />
+        <label htmlFor="size">Size:</label><br />
+        <select name="size" ref={sizeSelectRef}>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
         </select><br /><br />
         <input type="submit" value="Generate" />
       </form>
